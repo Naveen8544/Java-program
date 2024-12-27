e22d0e0f5ae9f0e7dc97b24e57535b07c91b08f8
Naveen Sharma <naveensharmaaurangabad@gmail.com>
2024-12-27 23:18:53 +0530

    Add files via upload
    Not completed: Lab_ANPC9434_Memberofclass_Oct21
    Assignment-1. ● 
    
    Create a class Student in Student.java then add member variables studentName, collegeName of type String 
    
    ● Add a member variable studentID of type int. 
    
    ● Make all the member variables as private. 
    
    ● Add a main method. And print a message “Successful”. 
    
    ● Compile the class 
    
    ● Run the class (Follow Coding convention)

---
+++ Student.java
@@ -0,0 +1,25 @@
+public class Student
+ {
+
+   
+ // Private member variables
+   
+    private String studentName;
+   
+    private String collegeName;
+   
+      private int studentID;
+
+   
+       // Main method
+  
+    public static void main(String[] args) 
+ {
+        
+    // Print success message
+       
+    System.out.println("Successful");
+   
+   }
+
+ }
